2f9ad53c990dc913c65c13ab0c3cbc1125b167a1
Tori Hedden <tori.hedden@gmail.com>
2022-03-11 12:09:55 -0500

    Correct time greeting control flow

--- src/Summary.jsx
+++ src/Summary.jsx
@@ -5,7 +5,7 @@ const greeting = () => {
 
   if (time < 12) {
     return "Good morning!";
-  } else if (time > 12 && time < 17) {
+  } else if (time >= 12 && time < 17) {
     return "Good afternoon!";
   } else return "Good evening!";
 };
