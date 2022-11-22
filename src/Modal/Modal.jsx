e6b85e218e110db0c59c60823f500ed49119a4ac
torihedden <tori.hedden@gmail.com>
2022-11-21 22:52:53 -0500

    Convert inputs to forms

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -17,26 +17,37 @@ const Modal = (props) => {
       <div className="modal-content">
         <div className="modal-prompt">{prompt}</div>
 
+        {/* for deletion modal */}
         {!isEditable && <div className="modal-text">{content}</div>}
-        {isEditable && (
-          <input
-            className="edit-task"
-            type="text"
-            onChange={(event) => {
-              setCurrentTodo({ ...currentTodo, title: event.target.value });
-            }}
-            id={currentTodo._id}
-            name={currentTodo.title}
-            value={content}
-            autoFocus
-          />
-        )}
-        <div className="button-wrapper">
-          <button onClick={cancelAction}>Cancel</button>
-          <button className="negative" onClick={confirmAction}>
-            {confirmText}
-          </button>
-        </div>
+
+        <form>
+          {/* for editing modal */}
+          {isEditable && (
+            <input
+              className="edit-task"
+              type="text"
+              onChange={(event) => {
+                setCurrentTodo({ ...currentTodo, title: event.target.value });
+              }}
+              id={currentTodo._id}
+              name={currentTodo.title}
+              value={content}
+              autoFocus={isEditable ? true : false}
+            />
+          )}
+
+          <div className="button-wrapper">
+            <button
+              onClick={cancelAction}
+              autoFocus={!isEditable ? true : false}
+            >
+              Cancel
+            </button>
+            <button className="negative" onClick={confirmAction}>
+              {confirmText}
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
--- src/TodoCreator/TodoCreator.jsx
+++ src/TodoCreator/TodoCreator.jsx
@@ -4,9 +4,7 @@ const TodoCreator = (props) => {
   const { handleAdd, newTodoTitle, setNewTodoTitle } = props;
 
   return (
-    <div className="new-task-wrapper">
-      {/* TODO: convert text input and button to proper form */}
-      {/* <form onSubmit={handleAdd}> */}
+    <form className="new-task-wrapper">
       <label htmlFor="new-todo">
         <input
           type="text"
@@ -19,7 +17,7 @@ const TodoCreator = (props) => {
       </label>
       <button
         className="positive add-todo"
-        // type="submit"
+        type="submit"
         onClick={() => {
           if (newTodoTitle !== "") {
             handleAdd({ title: newTodoTitle.trim() });
@@ -30,8 +28,7 @@ const TodoCreator = (props) => {
       >
         Add
       </button>
-      {/* </form> */}
-    </div>
+    </form>
   );
 };
 
